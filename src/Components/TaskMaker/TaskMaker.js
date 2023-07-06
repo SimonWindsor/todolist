10c505030e73d8e0ac273f7258ceec1619d02651
Simon Windsor <s3918370@student.rmit.edu.au>
2023-07-06 14:11:29 +1000

    Compents created

--- src/Components/TaskMaker/TaskMaker.js
+++ src/Components/TaskMaker/TaskMaker.js
@@ -4,6 +4,16 @@ import Task from '../Task/Task';
 import Subtask from '../Subtask/Subtask';
 
 class TaskMaker extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.createTask = this.createTask.bind(this);
+  }
+
+  createTask() {
+    return
+  }
+  
   render() {
     return(
       <div className="task-maker">
