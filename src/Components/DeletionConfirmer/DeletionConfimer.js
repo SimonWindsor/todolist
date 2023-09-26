be52ad300eba2931e9c9c396526bae1fec40d318
Simon <simon.nicholas@outlook.com>
2023-09-26 18:41:59 +1000

    Deletion confirmer styled

--- src/Components/DeletionConfirmer/DeletionConfimer.js
+++ src/Components/DeletionConfirmer/DeletionConfimer.js
@@ -7,8 +7,8 @@ class DeletionConfirmer extends React.Component {
     return (
       <div id="deletion-confirmer" hidden>
         <h3>Confirm Delete?</h3>
-        <button className="blue-btn" onClick={this.props.onDelete}>Yes</button>
-        <button className="blue-btn" onClick={this.props.onCancel}>No</button>
+        <button className="blue-btn yes-no" onClick={this.props.onDelete}>Yes</button>
+        <button className="blue-btn yes-no" onClick={this.props.onCancel}>No</button>
       </div>
     )
   }
